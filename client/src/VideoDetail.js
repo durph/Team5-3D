0081fe313b5e008c7416f21d6a74489f4c63c9b3
root <root@LAPTOP-HBCTG0EI.localdomain>
2019-05-24 16:01:14 -0400

    fixes

--- client/src/VideoDetail.js
+++ client/src/VideoDetail.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import './VideoDetails.scss'
+// import React from 'react';
+// import './VideoDetails.scss'
 
-function VideoDetails() {
-    return (
-      <div className="VideoDetails">
-        <div className="videoContainer">
-            <div className="video-title">
-                <h1 className = "video-title__title">
-                    AVERTING THE CLIMATE CRISIS
-                </h1>
-                <h2 className="video-title__speaker">
-                    Al Gore
-                </h2>
-            </div>
-            <div className="video-button">
-                WATCH VIDEO
-            </div>
+// function VideoDetails() {
+//     return (
+//       <div className="VideoDetails">
+//         <div className="videoContainer">
+//             <div className="video-title">
+//                 <h1 className = "video-title__title">
+//                     AVERTING THE CLIMATE CRISIS
+//                 </h1>
+//                 <h2 className="video-title__speaker">
+//                     Al Gore
+//                 </h2>
+//             </div>
+//             <div className="video-button">
+//                 WATCH VIDEO
+//             </div>
 
-        </div>
-        <div className="video-info">
-            <h3 className="video-info__details">Details<p className="video-info__details--blurb">About the talk</p></h3>
-            <h3 className="video-info__transcript">Transcript<p className="video-info__transcript--blurb">1 language</p></h3>
-            <h3 className="video-info__comments">Comments<p className="video-info__comments--blurb">Join the Conversation</p></h3>
-        </div>
-        <div className="video-info__duration">
-            16 minutes 17 seconds
-        </div>
-        <div className="video-info__views">
-            43,155,405 views
-        </div>
-        <div className="video-info__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </div>
-      </div>
-    );
-  }
+//         </div>
+//         <div className="video-info">
+//             <h3 className="video-info__details">Details<p className="video-info__details--blurb">About the talk</p></h3>
+//             <h3 className="video-info__transcript">Transcript<p className="video-info__transcript--blurb">1 language</p></h3>
+//             <h3 className="video-info__comments">Comments<p className="video-info__comments--blurb">Join the Conversation</p></h3>
+//         </div>
+//         <div className="video-info__duration">
+//             16 minutes 17 seconds
+//         </div>
+//         <div className="video-info__views">
+//             43,155,405 views
+//         </div>
+//         <div className="video-info__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+//             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+//             aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+//             nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+//         </div>
+//       </div>
+//     );
+//   }
   
-  export default VideoDetails;
+//   export default VideoDetails;
   
